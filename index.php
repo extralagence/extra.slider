00c07263312fa4cb1e1ab4b749b5430a1f301d2e
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2014-03-17 15:15:31 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,278 @@
+<?php
+    
+    $items = array(
+        '<li>
+            <img src="img/gallery/img6.jpg" alt="" width="900" height="500">
+            <p>
+                <a href="http://bit.ly/1b3A4fn">Download</a>&nbsp;&#8226;&nbsp;By&nbsp;<a href="http://www.nilssonlee.se">Jonas Nilsson Lee</a>
+            </p>
+        </li>',
+        '<li>
+            <img src="img/gallery/img3.jpg" alt="" width="900" height="500">
+            <p>
+                <a href="http://bit.ly/1epg2cd">Download</a>&nbsp;&#8226;&nbsp;By&nbsp;<a href="http://www.fanq.pt">Diogo Tavares</a>
+            </p>
+        </li>',
+        '<li>
+            <img src="img/gallery/img2.jpg" alt="" width="900" height="500">
+            <p>
+                <a href="http://bit.ly/K6nD6G">Download</a>&nbsp;&#8226;&nbsp;By&nbsp;<a href="http://www.carlsson.it">Elias Carlsson</a>
+            </p>
+        </li>',
+        '<li>
+            <img src="img/gallery/img1.jpg" alt="" width="900" height="500">
+            <p>
+                <a href="http://bit.ly/H75Z08">Download</a>&nbsp;&#8226;&nbsp;By&nbsp;<a href="http://www.florianklauer.de">Florian Klauer</a>
+            </p>
+        </li>'
+    );
+    	
+?><!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="no-js ie ie6 lte7 lte8 lte9"><![endif]-->
+<!--[if IE 7 ]><html class="no-js ie ie7 lte7 lte8 lte9"><![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8 lte8 lte9"><![endif]-->
+<!--[if IE 9 ]><html class="no-js ie ie9 lte9 recent"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html class="recent noie no-js"><!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+
+		<title>Extra Slider</title>
+
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/extra.slider.css">
+		<link rel="stylesheet" href="css/sliders.css">
+
+        <!-- IE9.js -->
+        <!--[if (gte IE 6)&(lte IE 8)]>
+        <link rel="stylesheet" href="css/ie.css" />
+        <script src="js/selectivizr-min.js"></script>
+        <script src="js/html5shiv.js"></script>
+        <![endif]-->
+
+	</head>
+
+	<body class="home">
+
+		<header class="header">
+			<h1 class="main-title">Extra Slider</h1>
+			<nav class="main-navigation">
+    			<ul>
+    				<li>
+    					<a href="#simple">Simple</a>
+    				</li>
+    				<li>
+    					<a href="#drag">Draggable</a>
+    				</li>
+    				<li>
+    					<a href="#keyboard">Keyboard</a>
+    				</li>
+    				<li>
+    					<a href="#margin">Margin</a>
+                    </li>
+                    <li>
+                        <a href="#fade">Fade</a>
+                    </li>
+                    <li>
+                        <a href="#multiple">Multiple</a>
+                    </li>
+                    <li>
+                        <a href="#automatic">Automatic</a>
+                    </li>
+    			</ul>
+			</nav>
+		</header>
+
+		<section class="main-content">
+			<article class="single-content" id="simple">
+			    
+				<div class="extra-slider">
+					<div class="wrapper">
+						<ul>
+						    <?php 
+                                shuffle($items);
+                                foreach($items as $item) {
+                                    echo $item;   
+                                }
+                            ?>
+						</ul>
+					</div>
+					<div class="navigation">
+						<a href="#" class="prev">Précédent</a>
+						<a href="#" class="next">Suivant</a>
+					</div>
+					<div class="pagination"></div>
+				</div>
+
+                <h2>Simple slider</h2>
+                <h3>Out of the box</h3>
+                
+                <div class="code">
+                    <iframe width="100%" height="600" src="http://jsfiddle.net/RVQmj/embedded/html,css,js" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
+                </div>
+
+			</article>
+
+			<article class="single-content" id="drag">
+			    
+				<div class="extra-slider">
+					<div class="wrapper">
+						<ul>
+                            <?php 
+                                shuffle($items);
+                                foreach($items as $item) {
+                                    echo $item;   
+                                }
+                            ?>
+						</ul>
+					</div>
+					<div class="navigation">
+						<a href="#" class="prev">Précédent</a>
+						<a href="#" class="next">Suivant</a>
+					</div>
+					<div class="pagination"></div>
+				</div>
+
+                <h2>Draggable slider</h2>
+                <h3>Let me touch you</h3>
+
+			</article>
+
+			<article class="single-content" id="keyboard">
+			    
+				<div class="extra-slider">
+					<div class="wrapper">
+						<ul>
+                            <?php 
+                                shuffle($items);
+                                foreach($items as $item) {
+                                    echo $item;   
+                                }
+                            ?>
+						</ul>
+					</div>
+					<div class="navigation">
+						<a href="#" class="prev">Précédent</a>
+						<a href="#" class="next">Suivant</a>
+					</div>
+					<div class="pagination"></div>
+				</div>
+
+                <h2>Keyboard slider</h2>
+                <h3>Let me press you</h3>
+
+			</article>
+
+			<article class="single-content" id="margin">
+			    
+				<div class="extra-slider">
+					<div class="wrapper">
+						<ul>
+                            <?php 
+                                shuffle($items);
+                                foreach($items as $item) {
+                                    echo $item;   
+                                }
+                            ?>
+						</ul>
+					</div>
+					<div class="navigation">
+						<a href="#" class="prev">Précédent</a>
+						<a href="#" class="next">Suivant</a>
+					</div>
+					<div class="pagination"></div>
+				</div>
+
+                <h2>Showing more tabs</h2>
+                <h3>Show me moar'</h3>
+
+			</article>
+
+			<article class="single-content" id="fade">
+
+				<div class="extra-slider">
+					<div class="wrapper">
+						<ul>
+                            <?php 
+                                shuffle($items);
+                                foreach($items as $item) {
+                                    echo $item;   
+                                }
+                            ?>
+						</ul>
+					</div>
+					<div class="navigation">
+						<a href="#" class="prev">Précédent</a>
+						<a href="#" class="next">Suivant</a>
+					</div>
+					<div class="pagination"></div>
+				</div>
+
+                <h2>Fade transitions</h2>
+                <h3>Fade me gently</h3>
+
+			</article>
+
+            <article class="single-content" id="multiple">
+
+                <div class="extra-slider">
+                    <div class="wrapper">
+                        <ul>
+                            <?php 
+                                shuffle($items);
+                                foreach($items as $item) {
+                                    $newItem = $item;
+                                    $newItem = str_replace('<img src="img/gallery/img', '<img src="img/gallery/thumb', $newItem);
+                                    $newItem = str_replace('width="900" height="500"', 'width="290" height="161"', $newItem);
+                                    echo $newItem;   
+                                }
+                            ?>
+                        </ul>
+                    </div>
+                    <div class="navigation">
+                        <a href="#" class="prev">Précédent</a>
+                        <a href="#" class="next">Suivant</a>
+                    </div>
+                    <div class="pagination"></div>
+                </div>
+
+                <h2>Multiple items at a time</h2>
+                <h3>All together</h3>
+
+            </article>
+
+            <article class="single-content" id="automatic">
+
+                <div class="extra-slider">
+                    <div class="wrapper">
+                        <ul>
+                            <?php 
+                                shuffle($items);
+                                foreach($items as $item) {
+                                    echo $item;   
+                                }
+                            ?>
+                        </ul>
+                    </div>
+                    <div class="navigation">
+                        <a href="#" class="prev">Précédent</a>
+                        <a href="#" class="next">Suivant</a>
+                    </div>
+                    <div class="pagination"></div>
+                </div>
+
+                <h2>Automatic slider</h2>
+                <h3>Look mom no hands</h3>
+
+            </article>
+
+		</section>
+
+		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.11.5/TweenMax.min.js"></script>
+        <script src="js/jquery.touchSwipe.min.js"></script>
+        <script src="js/flexie.min.js"></script>
+		<script src="js/extra.slider.js"></script>
+		<script src="js/main.js"></script>
+
+	</body>
+</html>
